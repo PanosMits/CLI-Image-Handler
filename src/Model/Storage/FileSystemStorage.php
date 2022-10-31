e5b011396a68564a33c9042f11d03dd43311e710
PanosMits <panag.mitseas@gmail.com>
2022-10-31 10:58:59 +0000

    Add .env

--- src/Model/Storage/FileSystemStorage.php
+++ src/Model/Storage/FileSystemStorage.php
@@ -31,7 +31,7 @@ class FileSystemStorage implements StorageInterface
     public function save(string $filePath): string
     {
         $imageUuid = Uuid::uuid4()->toString();
-        $destinationPath = __DIR__.'/../../../storage/';
+        $destinationPath = __DIR__.getenv('LOCAL_DIRECTORY');
         try {
             if (!is_dir($destinationPath)) {
                 if (!mkdir($destinationPath, 0777, true)) {
