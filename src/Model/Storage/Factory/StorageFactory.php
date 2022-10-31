e67e9692fc2464a48d0e435764ee572b0d4d1da0
PanosMits <panag.mitseas@gmail.com>
2022-10-31 19:17:09 +0000

    Remove comments

--- src/Model/Storage/Factory/StorageFactory.php
+++ src/Model/Storage/Factory/StorageFactory.php
@@ -8,12 +8,10 @@ use Panosmits\Basekit\Model\Storage\StorageInterface;
 
 class StorageFactory
 {
-    // TODO: Find a way to create the correct instance without the need of If statements, just by using the $storage
     public static function createFromInput(string $storage): StorageInterface
     {
         $lowerCaseStorage = strtolower($storage);
         if ($lowerCaseStorage === strtolower(StorageEnum::DEFAULT_STORAGE->value)) return FileSystemStorage::make();
-        //if ($lowerCaseStorage === strtolower(StorageEnum::S3->value)) return S3Storage::make();
 
         // As default - Normally, the program will exit before reaching here if the storage provided is not supported.
         // Adding the following line to stop IDE from complaining.
--- src/Validators/StorageSupportValidator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Panosmits\Basekit\Validators;
-
-class StorageSupportValidator
-{
-
-}
